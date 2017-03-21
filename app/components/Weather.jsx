5eead31880adf4a7f9fa9300cf08b66068ec512b
Dustin <dustin.sappier@bluespurs.com>
2017-03-21 14:29:43 -0300

    added SASS & nav search functionality

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,7 +15,9 @@ var Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
      });
 
     openWeatherMap.getTemp(location).then(function (temp) {
@@ -30,6 +32,22 @@ var Weather = React.createClass({
         errorMessage: e.message
       });
     });
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function () {
     var {isLoading, location, temp, errorMessage} = this.state;
@@ -51,7 +69,7 @@ var Weather = React.createClass({
     }
     return (
       <div>
-        <h1 className="text-center">Weather component</h1>
+        <h1 className="text-center page-title">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
         {renderError()}
